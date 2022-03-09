8086dd7fce8d19a9478aaad704527764e59dcf77
Mathieu <studio.creatic@gmail.com>
2022-03-09 22:46:43 +0100

    config front vers back

--- src/components/Galery/Index.js
+++ src/components/Galery/Index.js
@@ -12,7 +12,7 @@ export default function Galery() {
     }, {
       encodeValuesOnly: true,
     });
-    const urlPortfolio = "http://localhost:1337/api/"
+    const urlPortfolio = "http://backend-charles-cantin.herokuapp.com/api/"
     const portfolios = `${urlPortfolio}portfolios?${query}`
     const [card, setCard] = useState([]);
     useEffect(() => {
@@ -65,7 +65,7 @@ export default function Galery() {
                         {card.map(({ attributes: item }) => (
                         <figure>
                             <span className="card-container" key={item.id}>
-                            <img className="card" src={`http://localhost:1337${item.img.data.attributes.url}`} />
+                            <img className="card" src={`http://backend-charles-cantin.herokuapp.com${item.img.data.attributes.url}`} />
                             </span>
                             <figcaption>
                             <div className="label-author">{item.title}</div>
